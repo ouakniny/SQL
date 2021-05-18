40286155cc637fa4966349e0a109529fa3b1b169
ouakniny <52452983+ouakniny@users.noreply.github.com>
2021-05-18 16:37:03 +0300

    Update reduce_contiguous_date_range.sql

--- reduce_contiguous_date_range.sql
+++ reduce_contiguous_date_range.sql
@@ -1,20 +1,11 @@
-with breaking as (
-	select PERSA, PERID_NUM,  PLANS
-		, BEGDA=convert(date,BEGDA)
-		, ENDDA=convert(date,iif(ENDDA=99991231,convert(varchar(8),getdate(),112),ENDDA))	
-		, breaking=iif(BEGDA=dateadd(day,1,lag(ENDDA) over (partition by PERSA, PERID_NUM,  PLANS order by BEGDA)),0,1)	
-	from [FlexStage].[dbo].[HR_439NSM_0001]
-	where PLANS not in (99999999) and [PERSK]='31' and PLANS not in (
-		select distinct ZZ_RESPOS
-		from [FlexStage].[dbo].[HR_439NSM_0001] 
-		where 20210518 between BEGDA and ENDDA and ZZ_RESPOS not in (00000000,99999999)
-	)
+with breaks as (
+	select PERNR, BEGDA, ENDDA, breaking=iif(BEGDA=dateadd(day,1,lag(ENDDA) over (partition by PERNR order by BEGDA)),0,1)	
+	from Interval
 ),
-breaking_cumulated as (
-	select *, cumul=sum(breaking) over (partition by PERSA, PERID_NUM,  PLANS order by BEGDA)
-	from breaking
+cumulative_breaks as (
+	select *, cumul=sum(breaking) over (partition by PERNR order by BEGDA)
+	from breaks
 )
-select PERSA, PERID_NUM,  PLANS, BEGDA=min(BEGDA), ENDDA=max(ENDDA)
-from breaking_cumulated
-group by PERSA, PERID_NUM,  PLANS, cumul
-having datediff(month, MIN(BEGDA), MAX(ENDDA))>=12 and MAX(ENDDA)=convert(date,getdate())
+select PERNR, BEGDA=min(BEGDA), ENDDA=max(ENDDA)
+from cumulative_breaks
+group by PERNR, cumul
